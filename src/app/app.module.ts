3dc609d37d8a3f1d9e95b3a694b01b3b104e97bc
owner <owner@ownernoMacBook-Air.local>
2021-04-10 19:41:42 +0900

    Add: hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //コンポーネントを生成すると自動で宣言が追加
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //アプリケーションに必要な外部モジュールのリスト
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
